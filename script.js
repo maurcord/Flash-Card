5ee36a9c3213b17bf3be12ea1e40038156c9a147
maurcord <67590886+maurcord@users.noreply.github.com>
2020-08-19 12:10:13 -0400

    add cocktail imgs

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let drinks = [
     {
     name:"Old Fashioned",
-    image: "img.jpg", 
+    image: "https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg", 
     style: "stirred", 
     ingredients: "sugar, water, bitters"
 },
 {
     name:"Daiquiri", 
-    image:"img.jpg", 
+    image:"https://www.thecocktaildb.com/images/media/drink/mrz9091589574515.jpg", 
     style: "shaken",
     ingredients:"sugar,lime,rum"
 },
 {   name:"Martini",
-    image:".jph",
+    image:"https://www.thecocktaildb.com/images/media/drink/71t8581504353095.jpg",
     style:"stirred",
     ingredients:"gin,vermouth,bitters"
 },{ name:"Collins",
-    image:".jpg",
+    image:"https://www.thecocktaildb.com/images/media/drink/qystvv1439907682.jpg",
     style: "highball",
     ingredients:"gin,soda,lime"
 },{
     name:"Manhattan",
-    image: ".jpg",
+    image: "https://www.thecocktaildb.com/images/media/drink/hz7p4t1589575281.jpg",
     style:"stirred",
     ingredients:"whiskey,vermouth,bitters"
 }
