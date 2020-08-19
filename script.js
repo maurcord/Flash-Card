ee6dc6be01a5881718146fb7db093d82844f8895
maurcord <67590886+maurcord@users.noreply.github.com>
2020-08-19 00:33:15 -0400

    save, added image and some html, css

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-let drinks = [{name:"Old Fashioned" ,image: "img.jpg", style: "Stirred", ingredients: "Sugar, water, bitters, whiskey"}, {name: "daiquiri"}]
+let drinks = [
+    {
+    name:"Old Fashioned",
+    image: "img.jpg", 
+    style: "stirred", 
+    ingredients: "sugar, water, bitters"
+},
+{
+    name:"Daiquiri", 
+    image:"img.jpg", 
+    style: "shaken",
+    ingredients:"sugar,lime,rum"
+},
+{   name:"Martini",
+    image:".jph",
+    style:"stirred",
+    ingredients:"gin,vermouth,bitters"
+},{ name:"Collins",
+    image:".jpg",
+    style: "highball",
+    ingredients:"gin,soda,lime"
+},{
+    name:"Manhattan",
+    image: ".jpg",
+    style:"stirred",
+    ingredients:"whiskey,vermouth,bitters"
+}
+];
+console.log(drinks[0])
