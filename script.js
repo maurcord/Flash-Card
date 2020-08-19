d81ada79b0d88945ddb5328717c41635b497be8b
maurcord <67590886+maurcord@users.noreply.github.com>
2020-08-19 14:15:11 -0400

    added green and red button functions

--- script.js
+++ script.js
@@ -27,6 +27,31 @@ let drinks = [
 }
 ];
 console.log(drinks[0].name)
+// making score an element, then changing score to integer with innerHTML
+let score = document.querySelector(".score")
+let currentScore = parseInt(score.innerHTML)
+let greenButton = document.querySelectorAll(".button-green")
+// Loops through all the green buttons, and adds event listener "click", 
+// and then we added a helper function too addScore(+10). 
+for(let i=0;i < greenButton.length;i++){
+greenButton[i].addEventListener("click", addScore)
+}
+function addScore (){
+    console.log(this)
+    currentScore += 10
+    score.innerHTML= currentScore
+}
+function deductScore (){
+    console.log(this)
+    currentScore -= 10
+    score.innerHTML= currentScore
+}
+// Loops through all the red buttons, and adds event listener "click"
+// and then we add helper function "deduct score" that will deduct 10 points
+let redButton = document.querySelectorAll(".button-red")
+for(let i=0;i<redButton.length;i++) {
+    redButton[i].addEventListener("click",deductScore)
+}
 
 let cardContainers = document.querySelectorAll(".cards") // This then becomes an array.
 console.log(cardContainers)
