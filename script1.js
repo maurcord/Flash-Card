1fb9a89c694fbd33921e568c95edee7240fee6dd
maurcord <67590886+maurcord@users.noreply.github.com>
2021-02-01 09:58:26 -0500

    updated UI, added css to cards

--- script1.js
+++ script1.js
@@ -5,23 +5,23 @@ let drinks = [
     name:"Old Fashioned",
     image: "https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg", 
     style: "stirred", 
-    ingredients: "2 ounces of Whiskey, 1/2 ounce of simple syrup, 2 dashes of 'Angostura Bitters'. Stir 20 times. Serve on ice. Garnish with orange twist. <br><br><br>The oldest recipe for this drink appears in George Kappeler's Modern American Drinks, published in 1895."
+    ingredients: "2 ounces of Whiskey, 1/2 ounce of simple syrup, 2 dashes of 'Angostura Bitters'. Stir 20 times. Serve on ice. Garnish with orange twist. <br><br>The oldest recipe for this drink appears in George Kappeler's Modern American Drinks, published in 1895."
 },
 {
     name:"Daiquiri", 
     image:"https://www.thecocktaildb.com/images/media/drink/mrz9091589574515.jpg", 
     style: "shaken",
-    ingredients:"2 ounces of Rum, 3/4 ounce of fresh lime juice, 3/4 ounce of simple syrup. Shake and strain, serve up in a coupe. Garnish with a lime wheel. <br><br><br>Named after the mining town in Cuba and championed by an American ex-pat engineer by the name of Jennings Cox in the early 1900's."
+    ingredients:"2 ounces of Rum, 3/4 ounce of fresh lime juice, 3/4 ounce of simple syrup. Shake and strain, serve up in a coupe. Garnish with a lime wheel. <br><br>Named after the mining town in Cuba and championed by an American ex-pat engineer by the name of Jennings Cox in the early 1900's."
 },
 {   name:"Martini",
     image:"https://www.thecocktaildb.com/images/media/drink/71t8581504353095.jpg",
     style:"stirred",
-    ingredients:"2 ounces of Gin, 1 ounce of dry vermouth, 2 dashes of orange bitters. Stir 20 times, serve up in a coupe. Garnish with a lemon twist or olives.<br><br><br> French or dry vermouth arrived in the United States in 1870's, and it was only natural that bartenders would start using this product as a subsitute for sweet or italian vermouth."
+    ingredients:"2 ounces of Gin, 1 ounce of dry vermouth, 2 dashes of orange bitters. Stir 20 times, serve up in a coupe. Garnish with a lemon twist or olives.<br>French or dry vermouth arrived in the United States in 1870's, and it was only natural that bartenders would start using this product as a subsitute for sweet or italian vermouth."
 },
 { name:"Collins",
     image:"https://www.thecocktaildb.com/images/media/drink/qystvv1439907682.jpg",
     style: "highball",
-    ingredients:"2 ounces of Gin, 3/4 ounce of fresh lemon juice, 3/4 ounce of simple syrup. Shake and strain over ice in highball, top with club soda.<br><br><br> First appeared in print in Jerry Thomas' Bartender Guide in 1876."
+    ingredients:"2 ounces of Gin, 3/4 ounce of fresh lemon juice, 3/4 ounce of simple syrup. Shake and strain over ice in highball, top with club soda.<br><br>First appeared in print in Jerry Thomas' Bartender Guide in 1876."
 },
 {   name:"Manhattan",
     image: "https://www.thecocktaildb.com/images/media/drink/hz7p4t1589575281.jpg",
@@ -31,45 +31,45 @@ let drinks = [
 ];
 console.log(drinks[0].name)
 // making score an element, then changing score to integer with innerHTML
-let score = document.querySelector(".score")
-let currentScore = parseInt(score.innerHTML)
-let greenButton = document.querySelectorAll(".button-green")
-// Loops through all the green buttons, and adds event listener "click", 
-// and then we added a helper function too addScore(+10). 
-for(let i=0;i < greenButton.length;i++){
-greenButton[i].addEventListener("click", addScore)
-}
-function addScore (){
-    // alert("Good Job!")
-    console.log(this)
-    currentScore += 20
-    score.innerHTML= currentScore
-    if(currentScore ===100){
-        alert("Congratulations, you passed! Cheers! You're now an official bartender! Please drink responsibly.")
-        resetScore()
-    }
-}
-function deductScore (){
-    // alert("Keep Studying!")
-    console.log(this)
-    currentScore -= 20
-    score.innerHTML= currentScore
-    if(currentScore === -100){
-        alert('You need to study!')
-        resetScore()
-    }
-}
-function resetScore () {
-    console.log(this)
-    currentScore === 0
-    score.innerHTML=currentScore
-}
-// Loops through all the red buttons, and adds event listener "click"
-// and then we add helper function "deduct score" that will deduct 10 points
-let redButton = document.querySelectorAll(".button-red")
-for(let i=0;i<redButton.length;i++) {
-    redButton[i].addEventListener("click",deductScore)
-}
+// let score = document.querySelector(".score")
+// let currentScore = parseInt(score.innerHTML)
+// let greenButton = document.querySelectorAll(".button-green")
+// // Loops through all the green buttons, and adds event listener "click", 
+// // and then we added a helper function too addScore(+10). 
+// for(let i=0;i < greenButton.length;i++){
+// greenButton[i].addEventListener("click", addScore)
+// }
+// function addScore (){
+//     // alert("Good Job!")
+//     console.log(this)
+//     currentScore += 20
+//     score.innerHTML= currentScore
+//     if(currentScore ===100){
+//         alert("Congratulations, you passed! Cheers! You're now an official bartender! Please drink responsibly.")
+//         resetScore()
+//     }
+// }
+// function deductScore (){
+//     // alert("Keep Studying!")
+//     console.log(this)
+//     currentScore -= 20
+//     score.innerHTML= currentScore
+//     if(currentScore === -100){
+//         alert('You need to study!')
+//         resetScore()
+//     }
+// }
+// function resetScore () {
+//     console.log(this)
+//     currentScore === 0
+//     score.innerHTML=currentScore
+// }
+// // Loops through all the red buttons, and adds event listener "click"
+// // and then we add helper function "deduct score" that will deduct 10 points
+// let redButton = document.querySelectorAll(".button-red")
+// for(let i=0;i<redButton.length;i++) {
+//     redButton[i].addEventListener("click",deductScore)
+// }
 
 let cardContainers = document.querySelectorAll(".cards") // This then becomes an array.
 console.log(cardContainers)
@@ -105,12 +105,13 @@ for (let i =0;i<cardContainers.length;i++){
     cardBack.appendChild(cardBackInformation)
     cardContainers[i].appendChild(cardBack)
     cardBack.style.height= "300px"
-    cardBack.style.width="210px"
-    cardBack.style.border="5px solid white"
-    cardBack.style.borderRadius="15px"
-    cardBack.style.display= "none"  
-    cardFront.style.borderRadius= "15px"
-    cardFront.style.border="5px solid white"
+    
+	cardBack.style.display= "none"  
+	// cardBack.style.backgroundColor= white
+	// cardBack.style.color=black;
+	cardFront.style.borderRadius= "10px"
+	cardFront.style.color= "white"
+    
     // Here were are adding images to front 'front' of the div.
     // We are using string interpolation to go through the drink array:"image"
     // and adding backgroundImages
@@ -118,7 +119,7 @@ for (let i =0;i<cardContainers.length;i++){
     cardFront.style.backgroundImage = `url(${drinks[i].image})`
     cardFront.style.backgroundSize = "cover"
     cardFront.style.height= "300px"
-    cardFront.style.width="210px"
+   
     cardFront.style.backgroundPosition= "center"
     cardFront.style.display= "block"
     cardFront.appendChild(cardInformation)
