553e668b2a45a31261a7aabf1acc03ac12c45004
maurcord <67590886+maurcord@users.noreply.github.com>
2021-02-05 12:26:59 -0500

    removed first page,removed old .jpgs

--- script.js
+++
@@ -1,5 +0,0 @@
-// let button = document.querySelector(".button")
-// let myFunction = function() {
-//     alert ("Are you 21? Press OK if you are of legal drinking age.")
-// }
-// button.addEventListener("click", myFunction);
